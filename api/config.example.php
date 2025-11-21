aa20fce62938c240c5aba31f7f0d63ca7ab6585d
Tyler Hill <tyler.hill@tacticdev.com>
2025-11-21 05:51:42 +0000

    chore: untrack api/config.php, add example and gitignore

---
+++ api/config.example.php
@@ -0,0 +1,8 @@
+<?php
+// Example config file. Copy to `api/config.php` and fill in your production DB credentials.
+return [
+    'DB_HOST' => 'srv995.hstgr.io',
+    'DB_NAME' => 'u792097907_slughouse',
+    'DB_USER' => 'u792097907_tdv',
+    'DB_PASS' => 'your_db_password_here'
+];
--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'DB_HOST' => 'srv995.hstgr.io',
-    'DB_NAME' => 'u792097907_slug_dev',
-    'DB_USER' => 'u792097907_slug_user',
-    'DB_PASS' => ';pC6vaIz',
-];
