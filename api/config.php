92746e28b87a2ffd5777347ff4f2e5e76e7028a6
Tyler Hill <tyler.hill@tacticdev.com>
2025-11-21 03:16:38 +0000

    fix: update config.php to correct DB and user for Hostinger deploy

--- api/config.php
+++ api/config.php
@@ -4,7 +4,7 @@
 
 return [
     'DB_HOST' => 'srv995.hstgr.io',
-    'DB_NAME' => 'u792097907_slughouse',
-    'DB_USER' => 'u792097907_tdv',
+    'DB_NAME' => 'u792097907_slug_dev',
+    'DB_USER' => 'u792097907_slug_user',
     'DB_PASS' => ';pC6vaIz',
 ];
